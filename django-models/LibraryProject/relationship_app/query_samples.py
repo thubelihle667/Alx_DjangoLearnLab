15eaabdbed6c6667f690b83c8746b3c387819a49
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-20 21:47:14 +0200

    Queryset fixed

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,6 +2,7 @@ python manage.py shell
 
 from relationship_app.models import Author,Book,Library,Librarian
 
-books_query_by_author = Book.objects.get(title='')
+books = Book.objects.get(name=library_name)
+books.all()
 all_books_list = Library.objects.all()
 librarian = Librarian.objects.get(name='')
