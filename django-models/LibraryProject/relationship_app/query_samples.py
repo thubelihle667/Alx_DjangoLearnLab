dafc385a801fc8e48668c606caa32e4a95c1f200
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-20 21:50:31 +0200

    Query fix 2

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ python manage.py shell
 
 from relationship_app.models import Author,Book,Library,Librarian
 
-books = Book.objects.get(name=library_name)
+books = Library.objects.get(name=library_name)
 books.all()
 all_books_list = Library.objects.all()
 librarian = Librarian.objects.get(name='')
