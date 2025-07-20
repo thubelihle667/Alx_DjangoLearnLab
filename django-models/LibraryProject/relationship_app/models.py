4866a28e8f14edffa1c0b7c960d44f58dff14041
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-20 21:42:42 +0200

    please

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
