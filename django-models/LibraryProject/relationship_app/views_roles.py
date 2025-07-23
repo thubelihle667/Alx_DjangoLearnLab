481cfa6bda34677dbedee2ffdf1914418970c19c
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-23 22:24:12 +0200

    orm

--- django-models/LibraryProject/relationship_app/views_roles.py
+++ django-models/LibraryProject/relationship_app/views_roles.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from .models import UserProfile
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
 from .forms import BookForm 
 
