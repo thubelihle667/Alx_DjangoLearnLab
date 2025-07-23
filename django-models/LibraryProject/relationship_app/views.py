43be49aa46792a4948d762fec7cb89ad21066b9c
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-23 20:08:06 +0200

    Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,10 +15,13 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'register/register.html'
-
-
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    return render(request, 'relationship_app/register.html', {'form': form})
 
