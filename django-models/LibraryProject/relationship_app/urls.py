4c711bbbfb16962d1b622ae44fe4d4544732496d
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-23 19:56:50 +0200

    views are viewing me

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import list_books
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('books/', views.list_books, name='book_list'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', LoginView.as_view(template_name='registration/login.html')),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html')),
+    path('register/', views.register, name='register'),
 ]
