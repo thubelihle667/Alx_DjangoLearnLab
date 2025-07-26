d3a872542e0650adad1203e419fbb8ec0c77fc2b
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-26 16:10:42 +0200

    advanced_features_and_security/ admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -9,9 +9,9 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("title", "author", "publication_year")
 
 admin.site.register(Book, BookAdmin)
-admin.site.register(CustomUser, UserAdmin)
 
-@admin.register(CustomUser)
+
+
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo',)}),
@@ -21,5 +21,5 @@ class CustomUserAdmin(UserAdmin):
     search_fields = UserAdmin.search_fields + ('date_of_birth', 'profile_photo',)
     list_filter = UserAdmin.list_filter + ('date_of_birth', 'profile_photo',)
 
-
+admin.site.register(CustomUser, UserAdmin)
 
