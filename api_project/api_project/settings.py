950e4c6bdefaa3e7a48e21b9190be299cb85a8ba
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-31 21:52:28 +0200

    Permissions in settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.TokenAuthentication',
     ],
-'DEFAULT_PERMISSION_CLASSEDDS':[
+'DEFAULT_PERMISSION_CLASSES':[
     'rest_framework.permission.IsAuthenticated',
 ]
 }
