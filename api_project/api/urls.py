4e1dc6efffdb9ffc7bf0557324f39491ce6bdc01
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-31 21:23:48 +0200

    Token Retrieval Endpoint check

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -8,6 +8,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
 
 
