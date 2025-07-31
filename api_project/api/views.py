6e6578a267fc86160d85a0312dacea9cb669af99
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-07-31 21:41:56 +0200

    Permissions Classes Check

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -13,5 +15,7 @@ class BookList(generics.ListAPIView):
         return books
 
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
