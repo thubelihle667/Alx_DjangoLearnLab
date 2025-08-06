2aa8f8adccb28683d123694ff3acfc810ff604f2
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-06 20:28:37 +0200

    Custom Serializers in DRF

---
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -0,0 +1,20 @@
+from rest_framewrok import serializers
+from .models import Author,Book
+
+class BookSerializer(serializer.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title','publication_year','author']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='author.username', read_only=True)
+    book = BookSerializer()
+
+    class Meta:
+        model = Author
+        fields = ['name', 'book']
+
+    def validate(self, data):
+        if publication_year > 2025:
+            raise serializers.ValidationError("Publication year cannot be in the future")
+        return data
