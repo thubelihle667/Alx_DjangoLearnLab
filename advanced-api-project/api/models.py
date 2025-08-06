2d966117b1245490d3947e29e0b877432912ea2b
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-06 21:49:33 +0200

    Custom Serializers

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Book(models. Model):
+class Book(models.Model):
     title = models.CharField(max_length=100)
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
