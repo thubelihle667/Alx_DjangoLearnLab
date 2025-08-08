a0fc036b230de0b4aa610effc06d3d291d5c5fde
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-08 20:27:22 +0200

    api/permissions.py and api/views.py ammendments

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-from .permissions import IsAuthenticatedOrReadOnly
+
 
 
 # Create your views here.
