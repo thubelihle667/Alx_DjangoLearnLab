7346828ac7286f3b473b19567f0a9f60b6acc4c7
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-08 20:04:05 +0200

    urls added

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from . import views
+from .views import [
+    CustomBookCreateView,
+    CustomBookDeleteView,
+    CustomBookDetailView,
+    CustomBookListView,
+    CustomBookUpdateView,
+]
 
 urlpatterns = [
-    path(),
-    path(),
-    path(),
-    path(),
-    path(),
+    path('books/', CustomBookListView.as_view(),name='list_books'),
+    path('books/<int:pk>/', CustomBookDetailView.as_view(), name='book_details'),
+    path('create_book/', CustomBookCreateView.as_view(), name='create_book'),
+    path('delete_book/', CustomBookDeleteView.as_view(), name='delete_book'),
+    path('update_book/', CustomBookUpdateView.as_view(), name='update_book'),
 ]
