3ae725f1da53c8df64a630ca1dcd431953692655
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-13 19:43:33 +0200

    Django Blog Project Setup and Configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'blog_user',
+        'PASSWORD': 'p@ssword123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
