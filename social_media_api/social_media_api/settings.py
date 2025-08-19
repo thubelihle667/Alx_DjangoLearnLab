36d98db40a602d1c57a4850b8ca84d394283dc2b
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-19 20:19:46 +0200

    User Authentication Setup

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from django.urls import include, path
+from 
+from django.urls import path, include
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
