1a48e4d644e0ee84b254f430d99506a33186c900
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-19 20:27:23 +0200

    User Authentication Setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.authentoken.models import Token
+from rest_framework.authtoken.models import Token
 from .models import User
 
 class UserPublicSerializer(serializers.ModelSerializer):
