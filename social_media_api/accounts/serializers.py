96a922706af0e734f2e027cabec22e5b9639e151
Thubelihle Ngcobo <thubelihlemd@gmail.com>
2025-08-19 20:36:18 +0200

    User Authentication Setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,17 +9,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()  
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        # Use the required create_user method
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        # Explicitly create a token
         Token.objects.create(user=user)
         return user
 
